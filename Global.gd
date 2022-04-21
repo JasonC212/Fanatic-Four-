68fc109d588aa088fed57e7fb297d7acf898bfa2
T Gibson <taylornicolegibson@gmail.com>
2022-04-21 13:02:39 -0400

    end scene added

--- Global.gd
+++ Global.gd
@@ -12,7 +12,7 @@ func update_current_time(t):
 	if time_left != null:
 		time_left.text = "Time Left : " + str(current_time)
 	if current_time <= 0:
-		get_tree().quit()
+		var _scene = get_tree().change_scene("res://End_Game.tscn")
 	
 func update_total_time(t):
 	total_time += t
